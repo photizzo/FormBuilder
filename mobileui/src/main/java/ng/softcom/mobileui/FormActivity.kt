69e9d47ef1d0f2e74679016ce4d4e37205d9dffa
Emayoung <emayoung95@gmail.com>
2018-12-29 20:42:25 +0100

    Add Main Form Page Activity UI

--- mobileui/src/main/java/ng/softcom/mobileui/FormActivity.kt
+++ mobileui/src/main/java/ng/softcom/mobileui/FormActivity.kt
@@ -1,12 +1,134 @@
 package ng.softcom.mobileui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import ng.softcom.mobileui.databinding.ActivityFormBinding
 
-class FormActivity : AppCompatActivity() {
+
+class FormActivity : FragmentActivity() {
+
+
+    lateinit var binding:ActivityFormBinding
+
+    /**
+     * @numberOfPages The number of pages to show in this form.
+     * @currentPage the currentPage user is on the form.
+     * todo:replace this with live data
+     */
+    private var numberOfPages = 3 //default is
+    private var currentPage = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_form)
+
+        //pager variable is the id of the viewpager in xml since using kotlin extensions no need for findviewbyId
+        binding.pager.setMyScroller() //enable smooth scroll on non-swipeableViewPager
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        binding.pager.adapter = pagerAdapter
+
+        initFormControlButtons()
+    }
+
+    override fun onBackPressed() {
+        if (binding.pager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            binding.pager.currentItem = binding.pager.currentItem - 1
+        }
+    }
+
+    private fun initFormControlButtons(){
+        //check if form is single page
+        when(currentPage){
+            1 -> {
+                binding.includedMiddlePageControl.parent.visibility = View.GONE
+                binding.includedLastPageControl.parent.visibility = View.GONE
+            }
+            numberOfPages -> {
+                binding.includedFirstPageControl.buttonNext.visibility = View.GONE
+                binding.includedMiddlePageControl.parent.visibility = View.GONE
+                //check if form is a single page form only show submit button
+                if(currentPage == 1 && numberOfPages == 1)
+                    binding.includedLastPageControl.buttonBack.visibility = View.GONE
+
+            }
+            else -> {
+                binding.includedFirstPageControl.buttonNext.visibility = View.GONE
+                binding.includedLastPageControl.parent.visibility = View.GONE
+            }
+        }
+
+        binding.includedFirstPageControl.buttonNext.setOnClickListener {
+            currentPage++
+            binding.pager.currentItem = currentPage
+        }
+        binding.includedMiddlePageControl.buttonBack.setOnClickListener {
+            currentPage--
+            binding.pager.currentItem = currentPage
+        }
+        binding.includedMiddlePageControl.buttonNext.setOnClickListener {
+            currentPage++
+            binding.pager.currentItem = currentPage
+        }
+        binding.includedLastPageControl.buttonBack.setOnClickListener {
+            currentPage--
+            binding.pager.currentItem = currentPage
+        }
+        binding.includedLastPageControl.buttonNext.setOnClickListener {
+            currentPage++
+            binding.pager.currentItem = currentPage
+        }
+
+        binding.textViewPageNumber.text = "$currentPage/$numberOfPages"
+
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int = numberOfPages
+
+        override fun getItem(position: Int): Fragment = FormFragment()
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle?) {
+        savedInstanceState ?: return
+
+        with(savedInstanceState) {
+            // Save whether the address has been requested.
+            putInt(NUM_PAGES_KEY, numberOfPages)
+            putInt(CURRENT_PAGE_KEY, currentPage)
+        }
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        savedInstanceState?: return
+
+        with(savedInstanceState){
+            numberOfPages = getInt(NUM_PAGES_KEY)
+            currentPage = getInt(CURRENT_PAGE_KEY)
+        }
+        super.onRestoreInstanceState(savedInstanceState)
+
+
+    }
+
+
+    companion object {
+        const val NUM_PAGES_KEY = "num_pages"
+        const val CURRENT_PAGE_KEY = "current_page"
     }
 }
---
+++ mobileui/src/main/java/ng/softcom/mobileui/views/NonSwipeableViewPager.kt
@@ -0,0 +1,43 @@
+package ng.softcom.mobileui.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.animation.DecelerateInterpolator
+import android.widget.Scroller
+import androidx.viewpager.widget.ViewPager
+
+
+class NonSwipeableViewPager @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null) : ViewPager(context, attrs) {
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+    //down one is added for smooth scrolling
+    fun setMyScroller() {
+        try {
+            val viewpager = ViewPager::class.java
+            val scroller = viewpager.getDeclaredField("mScroller")
+            scroller.isAccessible = true
+            scroller.set(this, MyScroller(context))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    inner class MyScroller(context: Context) : Scroller(context, DecelerateInterpolator()) {
+
+        override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+            super.startScroll(startX, startY, dx, dy, 350 /*1 secs*/)
+        }
+    }
+}
