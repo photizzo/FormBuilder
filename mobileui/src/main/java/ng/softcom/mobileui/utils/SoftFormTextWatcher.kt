4cc54ca7812a1f3d75e9bd6b82e00adbad4cd655
Emayoung <emayoung95@gmail.com>
2019-01-03 10:18:10 +0100

    clean up Softformwatcher class

--- mobileui/src/main/java/ng/softcom/mobileui/utils/SoftFormTextWatcher.kt
+++ mobileui/src/main/java/ng/softcom/mobileui/utils/SoftFormTextWatcher.kt
@@ -2,7 +2,6 @@ package ng.softcom.mobileui.utils
 
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 
 
 abstract class SoftFormTextWatcher : TextWatcher {
@@ -19,32 +18,11 @@ abstract class SoftFormTextWatcher : TextWatcher {
         isRunning = true
         var sub = s.toString().getFormattedNumber(mask!!)
 
-//        var editableLength = s.toString().length
-//        if (editableLength < mask!!.length) {
-//            if (mask!!.toCharArray()[editableLength] != '#') {
-//                sub += mask!!.toCharArray()[editableLength]
-////                s!!.append(mask!!.toCharArray()[editableLength])
-//            } else if (mask!!.toCharArray()[editableLength] != '#') {
-//                sub = StringBuilder(sub).insert(editableLength - 1, mask, editableLength - 1, editableLength).toString()
-////                s!!.insert(editableLength - 1, mask, editableLength - 1, editableLength)
-//            }
-//        }
-//        s!!.replace(0, editableLength, sub)
         val text = sub
         s!!.clear()
         s!!.append(text)
 
         isRunning = false
-        Log.e("tag", "after $sub")
-
-//        val pattern = mask!!.replace("-", ",")
-//        val formatter = DecimalFormat(pattern)
-//        val yourFormattedString = formatter.format(s.toString().removeDashes().toLong())
-//        Log.e("tag", "after ${yourFormattedString.removeCommas()}")
-//
-//        updateNumberFormat(sub)
-
-
     }
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
